97a84dcc3de6c40c4f59e65bc19884edec278624
Rob Dawson <robert@rojotek.com>
2013-03-21 16:26:35 +1000

    removed incorrectly default sitemap setting which was never used.

--- lib/cobweb.rb
+++ lib/cobweb.rb
@@ -53,8 +53,7 @@ class Cobweb
     default_valid_mime_types_to                ["*/*"]
     default_cache_manager_to                   "DummyCache"
     default_crawl_limit_to                     100
-    default_use_sitemap_file                   true
-    
+
     puts @options
     @cache_manager = instanciate(@options[:cache_manager], @options)
     
